bad7d7db4968e2ec7fba9b9bb22a3cc3f7a94aa0
DDR19 <ddr.dan98@gmail.com>
2023-03-28 22:54:58 +0200

    Update NodeJS

--- service/routes.py
+++ service/routes.py
@@ -57,13 +57,13 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
+
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
-
 @app.route("/accounts", methods=["GET"])
 def list_account():
-    """ 
+    """
     List all the accounts stored
     This endpoint will return all the accounts stored
     """
@@ -73,13 +73,14 @@ def list_account():
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
 
+
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def read_account(account_id):
-    """ 
-    Read an unique account 
+    """
+    Read an unique account
     This endpoint will return a single account based on account_id
     """
     app.logger.info("Request to read an Account with id: %s", account_id)
@@ -88,13 +89,13 @@ def read_account(account_id):
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     return account.serialize(), status.HTTP_200_OK
 
+
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
-
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_account(account_id):
-    """ 
+    """
     Update existing account
     This endpoint will update existing account
     """
@@ -106,13 +107,13 @@ def update_account(account_id):
     account.update()
     return account.serialize(), status.HTTP_200_OK
 
+
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
-
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_account(account_id):
-    """ 
+    """
     Delete existing account
     This endpoint will delete existing account
     """
@@ -127,8 +128,6 @@ def delete_account(account_id):
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
-
-
 def check_content_type(media_type):
     """Checks that the media type is correct"""
     content_type = request.headers.get("Content-Type")
